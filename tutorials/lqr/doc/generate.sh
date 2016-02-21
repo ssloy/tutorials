9da1c3f10511b4002922fa53b59c414bf607793e
ssloy <spam@skisa.org>
2016-02-21 15:01:30 +0100

    doc

--- tutorials/lqr/doc/generate.sh
+++ tutorials/lqr/doc/generate.sh
@@ -2,7 +2,7 @@
 for i in `ls -1 f*tex`; do
     rubber $i
     dvisvgm --no-fonts --scale=1.5 `basename $i tex`dvi
-    convert -density 100 `basename $i tex`svg `basename $i tex`png
+    convert -density 85 `basename $i tex`svg `basename $i tex`png
     rubber --clean $i
 done
 

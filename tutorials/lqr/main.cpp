22b5811ab10720edd814f36fb5a25dc8a63fea62
ssloy <dmitry.sokolov@univ-lorraine.fr>
2016-04-08 10:38:44 +0200

    LQR with friction

--- tutorials/lqr/main.cpp
+++ tutorials/lqr/main.cpp
@@ -3,13 +3,13 @@
 #include "OpenNL_psm.h"
 
 int main() {
-    const int N = 2500; // 5 seconds
+    const int N = 1000; // 2 seconds
 
     const double x0 = 24.5; // 24.5 cm from the goal
     const double v0 = 0;   // zero initial speed, cm/sec
 
     nlNewContext();
-    nlSolverParameteri(NL_NB_VARIABLES, N*3);
+    nlSolverParameteri(NL_NB_VARIABLES, N*4);
     nlSolverParameteri(NL_LEAST_SQUARES, NL_TRUE);
 
     nlBegin(NL_SYSTEM);
@@ -20,47 +20,47 @@ int main() {
     nlLockVariable(1);
     nlSetVariable(1, v0);
 
-    nlLockVariable((N-1)*3);
-    nlSetVariable((N-1)*3, 0);
+    nlLockVariable((N-1)*4);
+    nlSetVariable((N-1)*4, 0);
 
-    nlLockVariable((N-1)*3+1);
-    nlSetVariable((N-1)*3+1, 0);
+    nlLockVariable((N-1)*4+1);
+    nlSetVariable((N-1)*4+1, 0);
 
-    nlLockVariable((N-1)*3+2);
-    nlSetVariable((N-1)*3+2, 0);
+    nlLockVariable((N-1)*4+2);
+    nlSetVariable((N-1)*4+2, 0);
 
     nlBegin(NL_MATRIX);
 
     for (int i=0; i<N-1; i++) {
         nlRowScaling(500);
         nlBegin(NL_ROW); // x{i+1} = xi + vi
-        nlCoefficient((i+1)*3  ,   -1);
-        nlCoefficient((i  )*3  ,    1);
-        nlCoefficient((i  )*3+1, .002);
+        nlCoefficient((i+1)*4  ,   -1);
+        nlCoefficient((i  )*4  ,    1);
+        nlCoefficient((i  )*4+1, .002);
         nlEnd(NL_ROW);
 
         nlRowScaling(500);
         nlBegin(NL_ROW); // v{i+1} = a vi + b ui
-        nlCoefficient((i+1)*3+1, -1);
-        nlCoefficient((i  )*3+1, 0.974);
-        nlCoefficient((i  )*3+2, 0.111808); // remember that x and v are measured in cm and cm/s
+        nlCoefficient((i+1)*4+1, -1);
+        nlCoefficient((i  )*4+1, 0.974);
+        nlCoefficient((i  )*4+2, 0.111808); // remember that x and v are measured in cm and cm/s
         nlEnd(NL_ROW);
     }
 
     for (int i=0; i<N; i++) {
         nlRowScaling(5);
         nlBegin(NL_ROW); // xi = 0, soft
-        nlCoefficient(i*3, 1);
+        nlCoefficient(i*4, 1);
         nlEnd(NL_ROW);
 
         nlRowScaling(1);
         nlBegin(NL_ROW); // vi = 0, soft
-        nlCoefficient(i*3+1, 1);
+        nlCoefficient(i*4+1, 1);
         nlEnd(NL_ROW);
 
         nlRowScaling(3);
         nlBegin(NL_ROW); // ui = 0, soft
-        nlCoefficient(i*3+2, 1);
+        nlCoefficient(i*4+2, 1);
         nlEnd(NL_ROW);
     }
 
@@ -69,12 +69,12 @@ int main() {
     nlSolve();
 
     std::vector<double> solution;
-    for (int i=0; i<3*N; i++) {
+    for (int i=0; i<4*N; i++) {
         solution.push_back(nlGetVariable(i));
     }
 
     for (int i=0; i<N; i++) {
-        std::cout << i*2 << " " << solution[i*3] << " " << solution[i*3+1] << " " << solution[i*3+2] << std::endl;
+        std::cout << i*2./1000. << " " << solution[i*4] << " " << solution[i*4+1] << " " << solution[i*4+2] << std::endl;
     }
 
 
@@ -90,9 +90,9 @@ int main() {
 
     for (int i=0; i<N; i++) {
         nlBegin(NL_ROW);
-        nlCoefficient(0, solution[i*3  ]/100); // bring position and speed back to m and m/s
-        nlCoefficient(1, solution[i*3+1]/100);
-        nlRightHandSide(solution[i*3+2]);
+        nlCoefficient(0, solution[i*4  ]/100); // bring position and speed back to m and m/s
+        nlCoefficient(1, solution[i*4+1]/100);
+        nlRightHandSide(solution[i*4+2]);
         nlEnd(NL_ROW);
     }
 

81ba09da52bfff667ceae21faf889fd0b13b57c8
Dmitry V. Sokolov <ha@haqr.eu>
2018-11-16 00:22:22 +0100

    convex

--- least-squares/1d/pic.py
+++ least-squares/1d/pic.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 
-f = [0.] * 32
-f[0] = f[31] = 1.
-f[18] = 2.
+x = [0.] * 32
+x[0] = x[31] = 1.
+x[18] = 2.
 
-plt.plot(f, drawstyle='steps-mid')
+plt.plot(x, drawstyle='steps-mid')
 
 for iter in range(1000):
-    for i in range(len(f)):
+    for i in range(len(x)):
         if i in [0,18,31]: continue
-        f[i] = (f[i-1]+f[i+1])/2.
+        x[i] = (x[i-1]+x[i+1])/2.
 
-plt.plot(f, drawstyle='steps-mid')
+plt.plot(x, drawstyle='steps-mid')
 plt.show()
 

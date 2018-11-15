a79d8f3f1ba0ddec5217971ff60bf28ce21d0bc1
ssloy <spam@skisa.org>
2018-11-15 16:37:56 +0100

    flat

---
+++ least-squares/1d/flat.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+f = [0.] * 32
+f[0] = f[-1] = 1.
+f[18] = 2.
+
+plt.plot(f, drawstyle='steps-mid')
+
+for iter in range(1000):
+    f[0] = f[1]
+    for i in range(1, len(f)-1):
+        f[i] = (f[i-1]+f[i+1])/2. 
+    f[-1] = f[-2]
+
+plt.plot(f, drawstyle='steps-mid')
+plt.show()
+
